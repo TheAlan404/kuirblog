3d06d05427e159e8b95800a266460240796d4e0c
TheAlan404 <gokdenizbektas@gmail.com>
2024-06-03 15:29:39 +0300

    unoptimized: true

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   distDir: "dist",
   output: "export",
   images: {
-    deviceSizes: [82, 110, 140, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    unoptimized: true,
   },
 };
 
